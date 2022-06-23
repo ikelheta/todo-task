a538800f737f068a0da6c7c94b231896976b05be
ikelheta <ibrahim.elheta@gmail.com>
2022-06-23 10:55:50 +0200

    dfsdc

---
+++ frontend/build/precache-manifest.fe085db06095966b6f94de0883f98963.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4feca8c6f92933c8a9aa7ad714b1dc24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f0d848f280efd66745f",
+    "url": "/static/css/main.1d66d08f.chunk.css"
+  },
+  {
+    "revision": "34d45bdcbd996de3d02a",
+    "url": "/static/js/2.c20de403.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/static/js/2.c20de403.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f0d848f280efd66745f",
+    "url": "/static/js/main.04133982.chunk.js"
+  },
+  {
+    "revision": "345e51f83483cb05013f",
+    "url": "/static/js/runtime-main.2eaee0d7.js"
+  }
+]);
