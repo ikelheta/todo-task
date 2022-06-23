79086c558bf3268d35b7eba514e3659d26a271c8
ikelheta <ibrahim.elheta@gmail.com>
2022-06-23 11:37:57 +0200

    dfsdc

--- src/app.ts
+++ src/app.ts
@@ -174,7 +174,7 @@ app.delete('/task/:id', isTokenValid, (req, res) => {
 })
 //--------------------------------------------------------------------------------------------------------------------------------------------------------
 if(process.env.NODE_ENV === "production"){
-  let reqFolder = path.join(__dirname, '../')
+  let reqFolder = path.join(__dirname, '../../')
   app.use(express.static(reqFolder + '/frontend/build'))
   app.get('*', (req, res)=>{
     res.sendFile(path.join(reqFolder +'/frontend/build/index.html'))
