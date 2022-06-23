f6f5827eebc583d51d3fa148280234fc7e82f586
ikelheta <ibrahim.elheta@gmail.com>
2022-06-23 13:08:43 +0200

    dscwe

--- src/app.ts
+++ src/app.ts
@@ -178,7 +178,7 @@ if(process.env.NODE_ENV === "production"){
 
   app.use(express.static(__dirname + '../frontend/build'))
   app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname +'..', 'frontend', 'build', 'index.html' ))
+    res.sendFile(path.join(__dirname +'..', 'frontend','test', 'build', 'index.html' ))
   })
 }else{
   app.get('/', (req, res)=>{
